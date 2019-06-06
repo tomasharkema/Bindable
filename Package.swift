13777f0663202e2b7900ea0ca7b9b81eb19bdb56
Tomas Harkema <tomas@harkema.io>
2019-06-06 10:05:24 +0200

    [WIP] just include all?!

--- Package.swift
+++ Package.swift
@@ -17,9 +17,10 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "Bindable",
-            path: "Sources/Core"),
+            path: "Sources"),
         .testTarget(
             name: "BindableTests",
-            dependencies: ["Bindable"]),
+            dependencies: ["Bindable"],
+            path: "BindableTests"),
     ]
 )
