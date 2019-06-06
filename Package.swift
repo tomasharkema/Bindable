4917ad2fdb28eb850665443a8e22455f40852367
Tomas Harkema <tomas@harkema.io>
2019-06-06 10:02:57 +0200

    fix path by only including core

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "Bindable",
-            path: "Sources"),
+            path: "Sources/Core"),
         .testTarget(
             name: "BindableTests",
             dependencies: ["Bindable"]),
